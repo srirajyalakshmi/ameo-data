836c785649e1d5c41a6ddbccdb8d60c092beb481
tammisetti Srirajyalakshmi <sriraji93@gmail.com>
2024-03-08 13:25:48 +0530

    initial commit

---
+++ day_5/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+notes = []
+@app.route('/', methods=['get',"POST"])
+def index():
+    if request.method=='POST':
+        note = request.form.get("note")
+        notes.append(note)
+    return render_template("home.html", notes=notes)
+
+
+@app.route('/clear')
+def clear():
+    notes.clear()
+    return render_template('home.html', notes=notes)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
+
+   
+   
+
+
+
+
